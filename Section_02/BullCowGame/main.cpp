4f0722c0428db7a3516d14bf962ec545a10cc219
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-08-14 15:55:21 -0300

    Use getline()

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
+#include<String>
+
+using namespace std;
 
 int main()
 {
+	//Introduce the game
 	constexpr int WORLD_LENGTH = 9;
 
-	std::cout << "Welcome to Bulls and Cows, a fun word game." << std::endl;
-	std::cout << "Can you guess the " << WORLD_LENGTH; 
-	std::cout << " letter isogram I'm thinking of?\n";
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORLD_LENGTH; 
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	//Get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	//Repeat the guess back to them
+	cout << "Your guess was: " << Guess << std::endl;
+
+	// try get a guess from the player without "string Guess = "";" 
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+
+	//Repeat the guess back to them
+	cout << "Your guess was: " << Guess << std::endl;
+	cout << endl;
 	return 0;
 }
