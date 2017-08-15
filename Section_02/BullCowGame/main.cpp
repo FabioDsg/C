206fab4e1998caae43055aa59517ce19d7d76a6c
FabioDsg <fabio.goncalves@catolica.edu.br>
2017-08-15 09:19:39 -0300

    for statement

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -9,33 +9,43 @@ string getGuessAndPrintBack();
 //The entry point for our application
 int main()
 {
-	
-	printIntro();
-	getGuessAndPrintBack();
 		
-	cout << endl;
+	printIntro();
+	
+	//Loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int i = 0; i < NUMBER_OF_TURNS; i++) 
+	{
+		getGuessAndPrintBack();
+		cout << endl;
+	}
+	
+		
 	return 0;
 }
 
-//Introduce the game
-void printIntro() {
 
-	constexpr int WORLD_LENGTH = 9;
+	//Introduce the game
+	void printIntro() 
+	{
+		constexpr int WORLD_LENGTH = 9;
+		cout << "Welcome to Bulls and Cows, a fun word game.\n";
+		cout << "Can you guess the " << WORLD_LENGTH;
+		cout << " letter isogram I'm thinking of?\n";
+		cout << endl;
+		return;
+	}
 
-	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORLD_LENGTH;
-	cout << " letter isogram I'm thinking of?\n";
-	cout << endl;
-	return;
-}
 
-//Get a guess from the player
-string getGuessAndPrintBack() {
-
-	cout << "Enter your guess: ";
-	string Guess = "";
-	getline(cin, Guess);
-	//Print the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-	return Guess;
+string getGuessAndPrintBack() 
+{
+		//Get a guess from the player
+		cout << "Enter your guess: ";
+		string Guess = "";
+		getline(cin, Guess);
+		
+		//Print the guess back to them
+		cout << "Your guess was: " << Guess << endl;
+		return Guess;
+		
 }
