d4ba0524590b29303c28f228503a0b96fca94936
Fábio <fdsgoncalves@outlook.com>
2020-09-25 20:44:20 -0300

    FString

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,14 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    PrintLine("Welcome to game! Let's play!");
+    PrintLine("Press Enter to continue...");
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = "cake";
 }
