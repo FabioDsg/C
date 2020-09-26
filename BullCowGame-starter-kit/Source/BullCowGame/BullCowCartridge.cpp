875f1596baa835f61a687970ef50d8318b72d5a1
Fábio <fdsgoncalves@outlook.com>
2020-09-26 11:03:55 -0300

    Fuctions

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,13 +5,52 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    PrintLine("Welcome to game! Let's play!");
-    PrintLine("Press Enter to continue...");
+    // Welcoming the player
+    PrintLine(TEXT("Welcome to game! Let's play!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove
+    PrintLine(TEXT("Press Enter to continue..."));
+
+    InitGame(); // Setting up game
     
+
+    
+
+    // Prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "cake";
+
+    // Check player guess
+    
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!!!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have lost!"));
+    }
+
+    // Check if Isogram
+
+    // Check Right number of characters
+
+    // Remove life
+
+    // Check if life is > 0
+
+    // If yes play again
+
+    // Show lives left
+
+    // Prompt to play again
+
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("cake"); // Declaration moved into head file
+    Lives = 4; // Set lives
 }
